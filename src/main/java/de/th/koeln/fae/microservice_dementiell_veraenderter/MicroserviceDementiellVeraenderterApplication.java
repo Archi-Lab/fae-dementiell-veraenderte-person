0c996a841bc98ad752ed866c6de115baf90bb7d3
Leonie Kallabis <leonie.kallabis@th-koeln.de>
2018-11-01 13:50:39 +0100

    initial push

---
+++ src/main/java/de/th/koeln/fae/microservice_dementiell_veraenderter/MicroserviceDementiellVeraenderterApplication.java
@@ -0,0 +1,12 @@
+package de.th.koeln.fae.microservice_dementiell_veraenderter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDementiellVeraenderterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDementiellVeraenderterApplication.class, args);
+    }
+}
